7913db29ea8d6bb41a9f754f8ca1b2ca2ebfbb28
Viktor Chee <kweic8@gmail.com>
2019-04-13 01:29:22 -0700

    added simple landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import './Landing.css';
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-        Landing Page
+      <div className="landing-container">
+        <div className="landing-header">
+          {/* TODO: Change to actual logo*/}
+          <div className="landing-logo">FaceLiftr</div>
+        </div>
+        <div className="landing-cards-container">
+          <div className="landing-card slogan">
+            <div>
+              Find your skincare regimen
+            </div>
+          </div>
+          <div className="landing-card recommendation">
+            Get immediate recommendations.
+          </div>
+          <div className="landing-card build">
+            Build a regimen.
+          </div>
+          <div className="landing-card customize">
+            Modify your choices.
+          </div>
+          <div className="landing-card track">
+            Track your skin progress.
+          </div>
+        </div>
+        <div></div>
       </div>
     );
   }
