d168be1c120d10cb68b2837b86ea1a5c9f82c902
Viktor Chee <kweic8@gmail.com>
2019-04-13 20:18:52 -0700

    removed some files in public and set constant route paths

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import Navbar from './Navbar';
 import Analytics from './Analytics';
 import FaceCapture from './FaceCapture';
 import Settings from './Settings';
+import * as ROUTES from '../constants/routes'
 
 class Home extends Component {
   render() {
@@ -13,9 +14,9 @@ class Home extends Component {
           <div className="mainNav">
             <Navbar />
             <Switch>
-              <Route path='/camera' component={FaceCapture} />
-              <Route path='/analytics' component={Analytics} />
-              <Route path='/settings' component={Settings} />
+              <Route path={ROUTES.FACECAPTURE} component={FaceCapture} />
+              <Route path={ROUTES.ANALYTICS} component={Analytics} />
+              <Route path={ROUTES.SETTINGS} component={Settings} />
             </Switch>
           </div>
         </div>
