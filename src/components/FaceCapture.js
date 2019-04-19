8a8bfa6e8290e3b286ccbed3cb4be81013d2f16d
Mohammed Almaroof <mohammedalmaroof33@gmail.comu>
2019-04-19 00:15:12 -0700

    fixed status 0 bug

--- src/components/FaceCapture.js
+++ src/components/FaceCapture.js
@@ -4,8 +4,6 @@ import $ from 'jquery';
 
 class FaceCapture extends React.Component { 
     clickInput = e => {
-        alert("lol");
-        alert(e);
         e.preventDefault();
         document.getElementById('input').click();
     } 
@@ -20,14 +18,10 @@ class FaceCapture extends React.Component {
 
     reader.onload = function (e) {
 
-        //移除之前的人脸框
         $("#facesContainer div").remove();
 
-        //图片的base64数据
         const base64Image = e.target.result;
 
-        //显示图片
-        //修复显示方向不对问题
         //fixOrientention(base64Image,imageView);
         const image = new Image();
         image.onload = () => {
@@ -41,19 +35,16 @@ class FaceCapture extends React.Component {
             const ctx = canvas.getContext('2d');
             ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-            // 旋转图片操作
             EXIF.getData(image, function () {
                 const orientation = EXIF.getTag(this, 'Orientation');
                 console.log(`orientation:${orientation}`);
                 switch (orientation) {
-                    // 正常状态
                     case 1:
                         console.log('旋转0°');
                         canvas.height = height;
                         canvas.width = width;
                         ctx.drawImage(image, 0, 0, width, height);
                         break;
-                    // 旋转90度
                     case 6:
                         console.log('旋转90°');
                         canvas.height = width;
@@ -62,7 +53,6 @@ class FaceCapture extends React.Component {
                         ctx.translate(0, -height);
                         ctx.drawImage(image, 0, 0, width, height);
                         break;
-                    // 旋转180°
                     case 3:
                         console.log('旋转180°');
                         canvas.height = height;
@@ -71,7 +61,6 @@ class FaceCapture extends React.Component {
                         ctx.translate(-width, -height);
                         ctx.drawImage(image, 0, 0, width, height);
                         break;
-                    // 旋转270°
                     case 8:
                         console.log('旋转270°');
                         canvas.height = width;
@@ -96,15 +85,12 @@ class FaceCapture extends React.Component {
         image.src = base64Image;
 
         /*
-        //base64方式上传图片
         let dic = {'image_base64' : base64Image};
 
         facepp.detectFace(dic,success,failed);
 
         */
 
-        // 以二进制的方式上传图片
-        // 将base64转为二进制
         //let imageData = facepp.dataURItoBlob(base64Image);
         const byteString = atob(base64Image.split(',')[1]);
         const mimeString = base64Image.split(',')[0].split(':')[1].split(';')[0];
@@ -114,20 +100,16 @@ class FaceCapture extends React.Component {
             ia[i] = byteString.charCodeAt(i);
         }
         let imageData = new Blob([ab], {type: mimeString});
-        //根据个人需求填写的参数,这里全部写上了,包括年龄性别等,详情看官方文档
         let attributes = 'gender,age,smiling,headpose,facequality,blur,eyestatus,emotion,ethnicity,beauty,mouthstatus,eyegaze,skinstatus';
-        //上传图片,获取结果
         let dataDic = {'image_file':imageData,'return_landmark':2,'return_attributes':attributes};
 
 
-        //调用接口，检测人脸
         const url = "https://api-us.faceplusplus.com/facepp/v3/detect";
         const formData = new FormData();
-
         formData.append('api_key','6zcDRWAnl78-csAxH_OR53WzJ87xHsfb');
         formData.append('api_secret','0_dzAPk2kZVbdiUvy_Tf0KXaGvkDRK8I');
 
-        for (var key in dataDic){//遍历拼接
+        for (var key in dataDic){
             formData.append(key,dataDic[key]);
         }
 
@@ -205,8 +187,8 @@ class FaceCapture extends React.Component {
             textView.innerText = JSON.stringify(e);
         }
         
-        alert(url);
-        $.ajax({
+        setTimeout(() => {
+          $.ajax({
             url: url,
             type: 'POST',
             cache: false,
@@ -214,8 +196,9 @@ class FaceCapture extends React.Component {
             data: formData,
             processData: false,
             contentType: false,
-            timeout: 100000000000000,//20秒超时时间
-        }).done(success).fail(failed);
+            timeout: 100000000000000,
+          }).done(success).fail(failed);
+        }, (1000));
     }
   }
 
